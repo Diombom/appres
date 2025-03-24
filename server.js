e8d5ed35404ac32131576323f0cb2681bf199a9c
diombom <brandondiom@gmail.com>
2025-03-24 11:58:41 -0600

    vercel setup

--- server.js
+++ server.js
@@ -112,3 +112,5 @@ app.post('/api/appointments', async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+module.exports = app;
